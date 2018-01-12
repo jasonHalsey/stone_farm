61c3b572c5d73e8e8c71941bdb0b6d66a446c411
Jason Halsey <jason@halsey-design.com>
2018-01-12 15:09:23 -0800

    WooCommerce Style Changes

--- stone_farm/functions.php
+++ stone_farm/functions.php
@@ -81,4 +81,11 @@ function sk_wcmenucart($menu, $args) {
 
 }
 
+// Add SVG Support
+// =============================================================================
 
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
